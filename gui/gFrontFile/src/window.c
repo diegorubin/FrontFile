e7017ef87a5c65d2cb18c9ad58e67847bbfd042d
Diego Rubin <rubin.diego@gmail.com>
2011-03-07 12:50:57 -0300

    Sourceview for search result in window.

--- gui/gFrontFile/src/window.c
+++ gui/gFrontFile/src/window.c
@@ -5,14 +5,54 @@ GtkWidget *create_main_window()
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "gFrontFile");
     gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER_ALWAYS);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
-    GtkWidget *table = gtk_table_new(2,2,FALSE);
+    GtkWidget *vbxWindow = gtk_vbox_new (FALSE, 0);
+    GtkWidget *tblMenu = gtk_table_new(2,2,FALSE);
+
+    gtk_box_pack_start (GTK_BOX (vbxWindow), 
+                        tblMenu,
+                        FALSE,
+                        FALSE,
+                        0); 
+
+    entPatterns = gtk_entry_new();
+    btnSearch = gtk_button_new_with_label("Pesquisar");
+
+    gtk_table_attach(GTK_TABLE(tblMenu), entPatterns, 0, 1, 0, 1, GTK_SHRINK,GTK_SHRINK,0,0);  
+    gtk_table_attach(GTK_TABLE(tblMenu), btnSearch, 1, 2, 0, 1, GTK_SHRINK,GTK_SHRINK,0,0);  
+
+
+    scwResult = gtk_scrolled_window_new (NULL, 
+                                         NULL); 
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scwResult), 
+                                  GTK_POLICY_AUTOMATIC, 
+                                  GTK_POLICY_AUTOMATIC); 
+
+    scvResult = gtk_source_view_new(); 
+    gtk_source_view_set_show_line_numbers (GTK_SOURCE_VIEW (scvResult), TRUE); 
+
+    gtk_container_add (GTK_CONTAINER (scwResult), scvResult); 
+
+    gtk_box_pack_start (GTK_BOX (vbxWindow), 
+                      scwResult, 
+                      TRUE, // vbox gives widget all remaining space 
+                      TRUE, // widget expands to fill given space 
+                      0); // pixel of padding around the widget 
+
+
+    gtk_container_add(GTK_CONTAINER(window), vbxWindow);
 
     /* signals connect */
-    g_signal_connect(G_OBJECT(window),"delete_event",G_CALLBACK(program_quit),NULL);
+    g_signal_connect(G_OBJECT(window),
+                     "delete_event",
+                     G_CALLBACK(program_quit),
+                     NULL);
+    
+    return window;
 }
 
-gboolean program_quit(GtkWidget *widget, GdkWvent *event, gpointer data)
+gboolean program_quit(GtkWidget *widget, GdkEvent *event, gpointer data)
 {
     gtk_main_quit();
     return TRUE;
--- gui/gFrontFile/src/window.h
+++ gui/gFrontFile/src/window.h
@@ -2,13 +2,21 @@
 #define _WINDOW_H_
 
 #include <gtk/gtk.h>
+#include <gtksourceview/gtksourceview.h>
 
 GtkWidget *window;
 GtkWidget *create_main_window();
 
 /* Window's widgets */
+/* entries */
+GtkWidget *entPatterns;
+
 /* buttons */
 GtkWidget *btnSearch;
+
+/* others */
+GtkWidget *scwResult;
+GtkWidget *scvResult;
 
 /* Callbacks */
 gboolean program_quit(GtkWidget *widget, GdkEvent *event, gpointer data);
